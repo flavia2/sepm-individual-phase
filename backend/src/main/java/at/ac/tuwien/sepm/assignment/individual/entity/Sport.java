6561ee006b62bfed4ef62cfea2de9c16dcca47c3
Flavia Helbig <e1625713@student.tuwien.ac.at>
2021-03-23 22:43:01 +0100

    [US7] Added description parameter to Sport

--- backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/entity/Sport.java
+++ backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/entity/Sport.java
@@ -1,10 +1,13 @@
 package at.ac.tuwien.sepm.assignment.individual.entity;
 
+import org.springframework.scheduling.support.SimpleTriggerContext;
+
 import java.util.Objects;
 
 public class Sport {
     private Long id;
     private String name;
+    private String description;
 
     public Sport() {
     }
@@ -16,6 +19,11 @@ public class Sport {
     public Sport(Long id, String name) {
         this(name);
         this.id = id;
+    }
+    public Sport(Long id, String name, String description) {
+        this(name);
+        this.id = id;
+        this.description = description;
     }
 
     public Long getId() {
@@ -34,13 +42,22 @@ public class Sport {
         this.name = name;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Sport sport = (Sport) o;
         return Objects.equals(id, sport.id) &&
-            Objects.equals(name, sport.name);
+            Objects.equals(name, sport.name) &&
+            Objects.equals(description, sport.description);
     }
 
     @Override
@@ -49,7 +66,7 @@ public class Sport {
     }
 
     protected String fieldsString() {
-        return "id=" + id + ", name='" + name + '\'';
+        return "id=" + id + ", name='" + name + '\'' + ", description='" + description +"\'";
     }
 
     @Override
