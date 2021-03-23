8a790948cd53cb310cdb333a9414ca71fb814a08
Flavia Helbig <e1625713@student.tuwien.ac.at>
2021-03-23 22:42:14 +0100

    [US7] Added description column in database

--- backend/src/main/resources/sql/createSchema.sql
+++ backend/src/main/resources/sql/createSchema.sql
@@ -1,20 +1,21 @@
 CREATE TABLE IF NOT EXISTS sport
 (
-  id         BIGINT AUTO_INCREMENT PRIMARY KEY,
-  name       VARCHAR(255) NOT NULL
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL,
+    description VARCHAR(500) NULL
 );
 
 CREATE TABLE IF NOT EXISTS Horse
 (
     id          BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name        VARCHAR(255)            NOT NULL,
-    description VARCHAR(500)            NULL,
+    name        VARCHAR(255)           NOT NULL,
+    description VARCHAR(500)           NULL,
     gender      ENUM('female', 'male') NOT NULL,
-    birthday    DATE                    NOT NULL,
-    sport       BIGINT                  NULL,
-    parentId1   BIGINT                  NULl,
-    parentId2   BIGINT                  NULL,
-    CONSTRAINT fk_sportId FOREIGN KEY (sport) REFERENCES sport(id) ON DELETE SET NULL,
-    CONSTRAINT fk_parentId1 FOREIGN KEY (parentId1) REFERENCES horse(id) ON DELETE SET NULL,
-    CONSTRAINT fk_parentId2 FOREIGN KEY (parentId2) REFERENCES horse(id) ON DELETE SET NULl
+    birthday    DATE                   NOT NULL,
+    sport       BIGINT                 NULL,
+    parentId1   BIGINT                 NULl,
+    parentId2   BIGINT                 NULL,
+    CONSTRAINT fk_sportId FOREIGN KEY (sport) REFERENCES sport (id) ON DELETE SET NULL,
+    CONSTRAINT fk_parentId1 FOREIGN KEY (parentId1) REFERENCES horse (id) ON DELETE SET NULL,
+    CONSTRAINT fk_parentId2 FOREIGN KEY (parentId2) REFERENCES horse (id) ON DELETE SET NULl
 );
