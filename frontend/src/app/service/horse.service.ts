d5fede008b536f61a273d7bc3f636a6cc86d0738
Flavia Helbig <e1625713@student.tuwien.ac.at>
2021-03-24 22:31:54 +0100

    [US1] Implemented horse service with getHorseById, getAllHorses and createHorse methods

---
+++ frontend/src/app/service/horse.service.ts
@@ -0,0 +1,36 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Horse} from '../dto/horse';
+import {environment} from 'src/environments/environment';
+
+const baseUri = environment.backendUrl + '/horses';
+const httpOptions = {
+  headers: new HttpHeaders({
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    'Content-Type': 'application/json'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HorseService {
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getHorseById(id: number): Observable<Horse> {
+    console.log('Load horse details for ' + id);
+    return this.httpClient.get<Horse>(baseUri + '/' + id);
+  }
+
+  getAllHorses(): Observable<Horse[]> {
+    console.log('Load all horses');
+    return this.httpClient.get<Horse[]>(baseUri);
+  }
+
+  createHorse(horse: Horse): Observable<Horse> {
+    console.log('Create new horse', horse);
+    return this.httpClient.post<Horse>(baseUri, horse, httpOptions);
+  }
+}
