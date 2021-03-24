ae2ea8e9baf5474016784521d3d60e9ad11f6325
Flavia Helbig <e1625713@student.tuwien.ac.at>
2021-03-24 22:11:05 +0100

    [US1] Changed header and added routing for horses

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,20 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {SportComponent} from './component/sport/sport.component';
+import {HorseComponent} from './component/horse/horse.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'sports', pathMatch: 'full'},
+  {path: '', redirectTo: 'horses', pathMatch: 'full'},
   {
     path: 'sports', component: SportComponent,
     children: [
       {path: 'add', component: SportComponent}
+    ]
+  },
+  {
+    path: 'horses', component: HorseComponent,
+    children: [
+      {path: 'add', component: HorseComponent}
     ]
   }
 
