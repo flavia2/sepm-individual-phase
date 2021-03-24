e8e01a4f056a4e5d53b1dacf867d325a7e02806d
Flavia Helbig <e1625713@student.tuwien.ac.at>
2021-03-24 21:56:27 +0100

    [US1,4] Implemented horse dto

---
+++ frontend/src/app/dto/horse.ts
@@ -0,0 +1,12 @@
+export class Horse {
+  constructor(
+    public id: number,
+    public name: string,
+    public description: string,
+    public birthday: string,
+    public gender: string,
+    public sport: number,
+    public parentId1: number,
+    public parentId2: number) {
+  }
+}
