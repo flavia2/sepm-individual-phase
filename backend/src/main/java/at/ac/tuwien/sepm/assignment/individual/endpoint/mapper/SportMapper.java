fa8637b5c20c0ba9acba0339fc92f0beb54eaf19
Flavia Helbig <e1625713@student.tuwien.ac.at>
2021-03-24 11:21:05 +0100

    [US7] Added dtoToEntity method

--- backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/endpoint/mapper/SportMapper.java
+++ backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/endpoint/mapper/SportMapper.java
@@ -4,13 +4,19 @@ import at.ac.tuwien.sepm.assignment.individual.endpoint.dto.SportDto;
 import at.ac.tuwien.sepm.assignment.individual.entity.Sport;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class SportMapper {
 
     public SportDto entityToDto(Sport sport) {
         if (sport == null)
             return null;
-        return new SportDto(sport.getId(), sport.getName());
+        return new SportDto(sport.getId(), sport.getName(), sport.getDescription());
+    }
+    public Sport dtoToEntity(SportDto sportDto) {
+        return new Sport(sportDto.getId(), sportDto.getName(), sportDto.getDescription());
     }
 
 }
