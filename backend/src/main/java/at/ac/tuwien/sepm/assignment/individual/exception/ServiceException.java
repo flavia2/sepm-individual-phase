a83d88c911d3afaa6ab6b02c662bd103b181c26e
Flavia Helbig <e1625713@student.tuwien.ac.at>
2021-03-31 12:14:39 +0200

    [TS24] Created ServiceException to wrap persistence exceptions

---
+++ backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/exception/ServiceException.java
@@ -0,0 +1,16 @@
+package at.ac.tuwien.sepm.assignment.individual.exception;
+
+public class ServiceException extends RuntimeException{
+
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(Throwable cause) {
+        super(cause);
+    }
+
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
